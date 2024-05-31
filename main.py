e07eb5b4d63c0cb707859b23d1f22e4ae4417167
nikit <nikitatokin2006@gmail.com>
2024-05-31 12:23:15 +0300

    добавил ограничение количества запросов в минуту на api по ip

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 from app.products.router import router as product_router
 from app.users.router import router as user_router
@@ -12,7 +16,13 @@ from app.auth import router as auth_router
 from app.file import router as file_router
 from app.config import settings
 
-app = FastAPI(title="Project", docs_url="/documentation", default_response_class=ORJSONResponse)
+app = FastAPI(title="Product Shop", docs_url="/documentation", default_response_class=ORJSONResponse)
+
+# ограничение количества запросов в минуту на api по ip
+limiter = Limiter(key_func=get_remote_address, default_limits=["45/minute"])
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 # список источников, на которые разрешено выполнять кросс-доменные запросы
 origins = [
@@ -36,10 +46,12 @@ app.include_router(auth_router)
 app.include_router(file_router)
 app.include_router(task_router)
 
+
 @app.get("/", tags=["Hello word"], name="hello-world")
 async def root():
     '''Приветственное сообщение'''
     return {"message": "Hello World"}
+
 
 @app.on_event("startup")
 async def startup_event():
