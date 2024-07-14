5f86cffd6b0d83698715db9065f0fe6933ecacd9
nikit <nikitatokin2006@gmail.com>
2024-07-14 15:13:00 +0300

    Обновил переменные окружения (убрал ненужные, добавил новые для доступа к s3 хранилищу)

--- app/config.py
+++ app/config.py
@@ -2,31 +2,28 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    '''Переменные окружения'''
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_DB: str
     POSTGRES_PORT: int
 
-    REDIS_HOST: str
-    REDIS_PORT: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+    S3_ENDPOINT_URL: str
+    S3_BUCKET_NAME: str
 
     @property
     def DATABASE_URL(self) -> str:
         '''Получение строки подключения к Postgres'''
-        return f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
+        return (f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
+                f'@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}')
 
-    @property
-    def REDIS_URL(self) -> str:
-        '''Получение строки подключения к Redis'''
-        return f'redis://{self.REDIS_HOST}:{self.REDIS_PORT}'
-
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
 settings = Settings()
