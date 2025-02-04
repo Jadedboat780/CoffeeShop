978c9c82eb7f2c922d470e004816d3b85c0ab65d
tokin_nikita <nikitatokin2006@gmail.com>
2025-02-04 20:38:42 +0300

    fix

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 from typing import Annotated
+
 from .database import get_session
+from .models import User, Coffee
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
-__all__ = (SessionDep,)
+__all__ = (User, Coffee, SessionDep)
