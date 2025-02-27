37d85e5f023bbc1c0e6e7c9bf2f194a727d1d5a1
tokin_nikita <nikitatokin2006@gmail.com>
2025-02-27 11:30:00 +0300

    updated CoffeeSlice

--- frontend/src/components/shared/CoffeeSlice/CoffeeSlice.tsx
+++ frontend/src/components/shared/CoffeeSlice/CoffeeSlice.tsx
@@ -1,11 +1,19 @@
+import {useCoffeeStore} from "@/store";
+import {useEffect} from "react";
 import {CoffeeCard} from "@/components/shared";
-import {ListCoffeeItem} from "@/types/coffee";
 import styles from './CoffeeSlice.module.css'
 
 
-export const CoffeeSlice = ({ coffeeList }: { coffeeList: ListCoffeeItem }) => {
+export const CoffeeSlice = () => {
+    const {coffeeList, getCoffeeList} = useCoffeeStore()
+    useEffect(() => {
+        getCoffeeList()
+    }, [])
+
     return <div className={styles.slice}>
-        {coffeeList.map((coffee) => (<CoffeeCard key={coffee.id} {...coffee} />))}
-        {coffeeList.map((coffee) => (<CoffeeCard key={coffee.id} {...coffee} />))}
+        {coffeeList ?
+            coffeeList.map((coffee) => <CoffeeCard key={coffee.id} {...coffee}/>)
+            : <span>Coffee no found :(</span>
+        }
     </div>
 }
