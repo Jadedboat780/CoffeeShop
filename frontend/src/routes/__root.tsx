be0eae5c0af0a908580de33f4a6377e617598066
tokin_nikita <nikitatokin2006@gmail.com>
2025-02-27 11:30:20 +0300

    fix

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,9 +2,9 @@ import {createRootRoute, Outlet} from '@tanstack/react-router'
 
 export const Route = createRootRoute({
     component: () => (
-        <>
+        <div>
             <Outlet/>
             {/*<TanStackRouterDevtools/>*/}
-        </>
+        </div>
     ),
 })
